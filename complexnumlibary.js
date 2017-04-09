87e5a050b31300ccafcffff353682901a91d4ac8
Anton Zotov <azpk360@yandex.ru>
2017-04-09 22:45:25 +0500

    Update complexnumlibary.js

--- complexnumlibary.js
+++ complexnumlibary.js
@@ -72,14 +72,58 @@ function cn_angle(num, values){
   return angle;
 }
 
+// Сложение комплексных чисел num1 на num2 и запись результата деления в res_num, sign - знак арифметической операции.
+function cn_arith(sign, num1, num2 ,res_num, values){
+  var re1 = values[num1][0];
+  var re2 = values[num2][0];
+  var im1 = values[num1][1];
+  var im2 = values[num2][1];
+  
+  if(sign == '+'){
+    values[res_num][0] = re1+re2;
+    values[res_num][1] = im1+im2;
+  }
+  if(sign == '-'){
+    values[res_num][0] = re1-re2;
+    values[res_num][1] = im1-im2;
+  }
+  
+  return values;
+}
+
+// Возведение комплексного числа в степень, num - число, возводимое в степень, n - степень числа, res_num - индекс для результата.
+function cn_pow(num, n, res_num, values){
+  var re = values[num1][0];
+  var im = values[num1][1];
+  
+  var val1 = math.pow(cp_module(re, im, 2), n)*(math.cos(cn_angle(re, im)*n)));
+  var val2 = math.pow(cp_module(re, im, 2), n)*(math.sin(cn_angle(re, im)*n)));
+  
+  values[res_num][0] = val1;
+  values[res_num][1] = val2;
+  
+  return value;
+}
+
 // Корень из комплексного числа, задается результирующий массив - nvalues, степень корня - i.
 function cn_sqrt(num, i, values, nvalues){
+  var re = values[num1][0];
+  var im = values[num1][1];
+  
   for(int k = 0; k < i; k++){
-    var val1 = cp_module(num, i, module)*(math.cos(cn_angle(num, values) + 2*i*k)/i));
-    var val2 = cp_module(num, i, module)*(math.sin(cn_angle(num, values) + 2*i*k)/i));
+    var val1 = cp_module(re, im, i+2)*(math.cos(cn_angle(re, im) + 2*i*k)/i));
+    var val2 = cp_module(re, im, i+2)*(math.sin(cn_angle(re, im) + 2*i*k)/i));
     
     nvalues[k][0] = val1;
     nvalues[k][1] = val2;
   }
   return nvalues;
+}
+
+// Найти противоположное значение комплексного числа.
+function cn_reverse(num, res_val, values){
+  values[res_val][0] = values[num][0];
+  values[res_val][1] = - values[num][1];
+  
+  return values;
 }
